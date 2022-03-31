2d3f7a62efe63b4897e274c20fcc5d0ef8f3291b
Hanafiah-Enovade <hanafiah@enovade.com>
2022-03-31 12:06:33 +0800

    update end-points port

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3030, () => {
-    console.log('Example app listening on port 3030!');
+app.listen(4000, () => {
+    console.log('Example app listening on port 4000!');
 });
 
 //Run app, then load http://localhost:3030 in a browser to see the output.
